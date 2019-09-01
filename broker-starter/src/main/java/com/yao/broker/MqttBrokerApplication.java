ec423dcf8846784836323efb46d5f580f4fe7656
噼里啪啦_cbin <497303054@qq.com>
2019-09-01 22:55:29 +0800

    启动服务

--- broker-starter/src/main/java/com/yao/broker/MqttBrokerApplication.java
+++ broker-starter/src/main/java/com/yao/broker/MqttBrokerApplication.java
@@ -1,9 +1,15 @@
 package com.yao.broker;
 
+import com.yao.broker.core.NettyServer;
 import com.yao.broker.core.server.IServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @Description:
@@ -11,13 +17,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2019/7/11 17:58
  */
 @SpringBootApplication
+@Controller
+@Slf4j
 public class MqttBrokerApplication {
     @Autowired
-    private static IServer nettyServer;
+    private NettyServer nettyServer;
+    @PostConstruct
+    public void startNetty(){
+        log.info("netty server begin to  start.");
+        nettyServer.start();
+        log.info("netty server started successful.");
+    }
     public static void main(String[] args) {
         try {
             SpringApplication.run(MqttBrokerApplication.class, args);
-            nettyServer.start();
             System.out.println("***************************************");
             System.out.println("***************************************");
             System.out.println("*******Platform  MQTT 启动成功*********");
@@ -25,7 +38,7 @@ public class MqttBrokerApplication {
             System.out.println("***************************************");
 
         } catch (Exception ex) {
-
+            ex.printStackTrace();
             System.out.println("***************************************");
             System.out.println(ex.getMessage());
             System.out.println("***************************************");
