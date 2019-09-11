c79ffe5e17c8fb5150c08b0abc5c779a0c54d1e2
噼里啪啦_cbin <497303054@qq.com>
2019-09-11 22:22:54 +0800

    添加日志追踪

--- broker-starter/src/main/java/com/yao/broker/MqttBrokerApplication.java
+++ broker-starter/src/main/java/com/yao/broker/MqttBrokerApplication.java
@@ -1,14 +1,9 @@
 package com.yao.broker;
 
-import com.yao.broker.core.NettyServer;
 import com.yao.broker.listener.ApplicationEventListener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @Description:
